ee699621ec0c0e5b1cce035add08e26d517cf925
javifranco0188-star <javifranco0188@gmail.com>
2026-09-03 22:42:10 +0200

    Actualizar caché v6

--- docs/sw.js
+++ docs/sw.js
@@ -1,5 +1,5 @@
-const CACHE="sorteo-oviedo-v5";
-const ASSETS=["./","./index.html","./manifest.webmanifest","./icon.svg","./portada-premio.jpg?v=5"];
+const CACHE="sorteo-oviedo-v6";
+const ASSETS=["./","./index.html","./manifest.webmanifest","./icon.svg","./portada-premio.jpg?v=6"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener("activate",e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
 self.addEventListener("fetch",e=>{
