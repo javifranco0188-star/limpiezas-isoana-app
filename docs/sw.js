ed78f678abd42c767bd9f4115d1709e5365b6155
javifranco0188-star <javifranco0188@gmail.com>
2026-09-03 21:20:51 +0200

    Actualizar caché de la app instalada

--- docs/sw.js
+++ docs/sw.js
@@ -1,12 +1,8 @@
-const CACHE="sorteo-oviedo-v1";
-const ASSETS=["./","./index.html","./manifest.webmanifest","./icon.svg"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+const CACHE="sorteo-oviedo-v2";
+const ASSETS=["./","./index.html","./manifest.webmanifest","./icon.svg","./portada-premio.jpg"];
+self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener("activate",e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
 self.addEventListener("fetch",e=>{
-  if(e.request.method!=="GET") return;
-  e.respondWith(fetch(e.request).then(r=>{
-    const copy=r.clone();
-    caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{});
-    return r;
-  }).catch(()=>caches.match(e.request).then(r=>r||caches.match("./"))));
+ if(e.request.method!=="GET")return;
+ e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{});return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match("./"))));
 });
