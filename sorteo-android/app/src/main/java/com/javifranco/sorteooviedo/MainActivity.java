29d694a52352a766b69c4caaca2a30eb5168b81f
javifranco0188-star <javifranco0188@gmail.com>
2026-09-03 20:09:59 +0200

    Usar nueva web funcional del sorteo

--- sorteo-android/app/src/main/java/com/javifranco/sorteooviedo/MainActivity.java
+++ sorteo-android/app/src/main/java/com/javifranco/sorteooviedo/MainActivity.java
@@ -16,7 +16,7 @@ import android.widget.Toast;
 public class MainActivity extends Activity {
     private static final int FILE_CHOOSER_REQUEST = 1001;
     private static final String APP_URL =
-            "https://javifranco0188-star.github.io/limpiezas-isoana-app/";
+            "https://jqmcpricmwzggleupxwp.supabase.co/functions/v1/sorteo-web";
 
     private WebView webView;
     private ValueCallback<Uri[]> fileCallback;
