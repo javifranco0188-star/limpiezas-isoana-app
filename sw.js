d6547ad5526167eedeb185b19e122f79abcaf116
javifranco0188-star <javifranco0188-star@users.noreply.github.com>
2026-09-05 15:29:48 +0000

    deploy: 83bee37c213d48d817257fd3e4efd17bad933fcf

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE="sorteo-oviedo-v6";
+const ASSETS=["./","./index.html","./manifest.webmanifest","./icon.svg","./portada-premio.jpg?v=6"];
+self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener("activate",e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
+self.addEventListener("fetch",e=>{
+ if(e.request.method!=="GET")return;
+ const req=e.request;
+ const isNav=req.mode==="navigate";
+ e.respondWith(
+   fetch(req,{cache:"no-store"}).then(r=>{
+     const copy=r.clone();
+     caches.open(CACHE).then(c=>c.put(req,copy)).catch(()=>{});
+     return r;
+   }).catch(()=>caches.match(req).then(r=>r||(isNav?caches.match("./index.html"):undefined)))
+ );
+});
